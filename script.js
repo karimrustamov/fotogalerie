d85fc21e74f76b99534f9064304937723a6ba000
Karim <karimrustamov@homail.de>
2023-09-09 12:11:27 +0200

    final

--- script.js
+++ script.js
@@ -19,38 +19,47 @@ const bilderArray = [
     'img18.jpg',
 ];
 
+let currentImageIndex = -1;
+
 function load() {
     const container = document.getElementById('allImages');
 
     for (let i = 0; i < bilderArray.length; i++) {
-       container.innerHTML += `
+        container.innerHTML += `
             <div class="imgbox">
-                <img onclick="openImage('./img/${bilderArray[i]}')" src="./img/${bilderArray[i]}" alt="img${i+1}">
+                <img onclick="openImage('./img/${bilderArray[i]}', ${i})" src="./img/${bilderArray[i]}" alt="img${i+1}">
             </div>
-       `;
+        `;
     }
 }
 
-function openImage(src) {
+function openImage(src, index) {
     const lightbox = document.getElementById('lightbox');
     const lightboxImg = document.getElementById('lightboxImg');
     
     lightboxImg.src = src;
-    lightbox.style.display = 'flex';  // Öffnen Sie die Lightbox
+    lightbox.style.display = 'flex'; 
+    currentImageIndex = index; 
+
     setTimeout(() => {
-        lightbox.style.opacity = '1'; // Setzt die Opazität auf 1, um die Lightbox anzuzeigen
-        lightboxImg.style.transform = 'scale(1)'; // Setzt die Skalierung des Bildes auf normal
+        lightbox.style.opacity = '1'; 
+        lightboxImg.style.transform = 'scale(1)'; 
     }, 50);
+}
+
+function switchImage(direction) {
+    currentImageIndex = (currentImageIndex + direction + bilderArray.length) % bilderArray.length;
+    openImage(`./img/${bilderArray[currentImageIndex]}`, currentImageIndex);
 }
 
 function closeLightbox() {
     const lightbox = document.getElementById('lightbox');
     const lightboxImg = document.getElementById('lightboxImg');
     
-    lightbox.style.opacity = '0'; // Setzt die Opazität auf 0, um die Lightbox auszublenden
-    lightboxImg.style.transform = 'scale(0.9)'; // Verkleinert das Bild leicht
-    
+    lightbox.style.opacity = '0'; 
+    lightboxImg.style.transform = 'scale(0.9)'; 
+
     setTimeout(() => {
-        lightbox.style.display = 'none';  // Verstecken Sie die Lightbox nachdem die Animation abgeschlossen ist
-    }, 500); // 500ms entsprechen der Dauer der CSS-Transition
+        lightbox.style.display = 'none';  
+    }, 500); 
 }
